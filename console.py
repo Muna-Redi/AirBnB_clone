311873384c8ca4b8d3fbae9a9ee4d09775543597
Muna-Redi <francischym@gmail.com>
2022-10-30 15:10:38 +0100

    making the console

---
+++ console.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" The console: A command interpreter implemented from class Cmd """
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ class inheriting from the Cmd class
+        Attributes:
+            prompt (str): a prompt to take input commands from user
+
+            intro (str): A text displayed before the prompt when the
+            console is run
+
+        Methods:
+            do_EOF: method to implement the End Of File and exit the program
+
+            do_quit: method to quit the program
+    """
+    prompt = "(hbnb)"
+
+    intro = "\n\nWELCOME TO hbnb CONSOLE Have Fun....\n\n"
+
+    def emptyline(self):
+        """ method to do nothing when an empty line is parsed """
+
+        pass
+
+    def do_quit(self, line):
+        """ command to quit the program """
+        print()
+
+        return True
+
+    def do_EOF(self, line):
+        """ this method recognises CTRL D and quits the program """
+        print()
+
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
